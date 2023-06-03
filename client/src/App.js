c56cccaaeaddf239e7750d019e90245db8353a86
Chiaoyun29 <chiaoyun29@yahoo.com>
2023-06-04 01:49:43 +0800

    logo

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,11 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import "./App.css";
-import logo from "../image/DECOT.png";
 
 const App = () => {
   return (
     <Router>
       <div>
-        <img src={logo} alt="logo" className="w-5/6 h-5/6 m-auto pt-5" />
         <nav>
           <ul>
             <li>
