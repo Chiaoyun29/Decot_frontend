7cfd50ed8c8ffb9bfd14316894dd9a44698823c5
Chiaoyun29 <chiaoyun29@yahoo.com>
2023-06-05 02:35:01 +0800

    canvas

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Canvas from './components/canvas/canvas_draft'
 import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
+    <><Canvas /><div className="App"> //need to figure out how to separate
       <div className="bg">
         <Router>
           <Routes>
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
           </Routes>
-          
+
           <nav>
             <ul className="list-none">
               <li>
@@ -26,7 +27,7 @@ const App = () => {
           </nav>
         </Router>
       </div>
-    </div>
+    </div></>
   );
 };
 
