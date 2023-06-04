31ea96ac18e904035d45a033e33fe63f1b630f0f
Chiaoyun29 <chiaoyun29@yahoo.com>
2023-06-04 16:29:54 +0800

    bg

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -22,22 +22,24 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <img src={logo} alt="logo" className="w-5/6 h-5/6 m-auto pt-5" />
-      <h1>Login</h1>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-      />
-      <button onClick={handleLogin}>Login</button>
+    <div className="bg-[url('/src/image/login_bg.jpg')] h-screen bg-no-repeat bg-center bg-cover">
+      <div className="flex flex-col w-1/3  text-center m-auto">
+        <img src={logo} alt="logo" className="w-5/6 h-5/6 m-auto pt-5" />
+        <h1>Login</h1>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button onClick={handleLogin}>Login</button>
+      </div>
     </div>
   );
 };
