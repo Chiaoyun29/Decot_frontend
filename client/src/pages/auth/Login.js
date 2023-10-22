7947d494ad35cd751eab219872b4663520b3f604
wasdqwerfzxcv <72960153+wasdqwerfzxcv@users.noreply.github.com>
2023-10-23 00:39:09 +0800

    add_google_auth

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
-import { loginUser } from '../../components/services/api';
+import React, { useState, useEffect } from 'react';
+import { loginUser, authenticateWithGoogle, updateUserRole } from '../../components/services/api';
 import { useNavigate } from 'react-router-dom';
 import CustomModal from '../../components/common/CustomModal';
 import logo from "../../image/DECOT.png";
 import { Link } from 'react-router-dom';
 import './auth.css';
 import { useAuthContext } from '../../context/AuthContext';
+import g_sign_in from  "../../image/google_sign_in.png";
 
 const Login = () => {
-    const { setUser, setToken } = useAuthContext();
+    const {user, setUser, setToken } = useAuthContext();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
+    const [message, setMessage] = useState('');
+    const [messageTitle, setMessageTitle] = useState('');
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const navigate = useNavigate();
+    const [roleModalIsOpen, setRoleModalIsOpen] = useState(false);
+    const [role, setRole] = useState('mentor'); 
 
     const handleLogin = async () => {
         try {
@@ -24,14 +28,72 @@ const Login = () => {
                 setToken(response.token); // set token in context
                 navigate("/dashboard");
             } else if (response.status === 401) {
-                setErrorMessage('Invalid credentials');
+                setMessageTitle('Error');
+                setMessage('Invalid credentials');
                 setModalIsOpen(true);
             } else {
-                setErrorMessage('Failed to login');
+                setMessageTitle('Error');
+                setMessage('Failed to login');
                 setModalIsOpen(true);
             }
         } catch (error) {
-            setErrorMessage('An error occurred while trying to log in');
+            setMessageTitle('Error');
+            setMessage('An error occurred while trying to log in');
+            setModalIsOpen(true);
+        }
+    };
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const token = urlParams.get('token');
+        const userJSON = urlParams.get('user');
+        console.log(token)
+        
+        // Check if both token and user parameters exist in the URL
+        if (token && userJSON) {
+            const user = JSON.parse(userJSON);
+            console.log(user)
+    
+            if (user && user.email) {
+                setUser(user);
+                setToken(token); 
+                if (user.role === null) {
+                    setRoleModalIsOpen(true);
+                } else {
+                    navigate("/dashboard");
+                }
+            } else {
+                setMessageTitle("Error!");
+                setMessage('Failed to get user data from Google');
+                setModalIsOpen(true);
+            }
+        }
+    
+    }, [navigate, setToken, setUser]);
+
+    const handleGoogleLogin = async () => {
+        authenticateWithGoogle();
+    };
+
+    const handleRoleSelection = async (selectedRole) => {
+        setRole(selectedRole);
+        setRoleModalIsOpen(false);
+
+        try {
+            const data = await updateUserRole(user.email, selectedRole);
+    
+            if (data) {
+                setMessageTitle("Registered Successfully");
+                setMessage('You have been registered successfully with your google account!');
+                setModalIsOpen(true);
+            } else {
+                setMessageTitle("Error!");
+                setMessage('Failed to set user role');
+                setModalIsOpen(true);
+            }
+        } catch (error) {
+            setMessageTitle("Error!");
+            setMessage('Failed to set user role');
             setModalIsOpen(true);
         }
     };
@@ -58,9 +120,26 @@ const Login = () => {
                     placeholder="Password"
                     className="block w-full mt-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 /><br />
-                <button onClick={handleLogin} className="w-full mt-4 px-4 py-2 text-white bg-indigo-500 rounded-md hover:bg-blue-700 focus:outline-none">
-                    Login
-                </button>
+                <div className="w-full flex justify-center mt-4">
+                    <button onClick={handleLogin} className="w-40 px-4 py-2 text-white bg-indigo-500 rounded-md hover:bg-blue-700 focus:outline-none">
+                        Login
+                    </button>
+                </div>
+               <div className="flex flex-col items-center mt-4 space-y-2">
+                    <div className="w-full h-px bg-gray-300"></div> 
+                    <p className="text-xs text-gray-600">Or Login With</p>
+                    <button 
+                        onClick={handleGoogleLogin} 
+                        className="mt-4 px-2 py-2 bg-white border border-gray-300 rounded-full hover:bg-blue-100 focus:outline-none"
+                    >
+                        <img 
+                            src={g_sign_in}
+                            alt="Login with Google"
+                            className="w-40 h-auto"  // Adjust w-24 to the width you want. h-auto will maintain aspect ratio.
+                        />
+                    </button>
+                </div>
+
                 <div className="mt-4 text-grey-600 text-xs">
                     Don't have an account?{" "}
                     <span>
@@ -72,9 +151,37 @@ const Login = () => {
                 <CustomModal
                     isOpen={modalIsOpen}
                     onClose={() => setModalIsOpen(false)}
-                    title="Error"
-                    message={errorMessage}
-                />
+                    title={messageTitle}
+                    message={message}
+                >
+                    {messageTitle === "Registered Successfully" && (
+                        <button
+                            className="mt-4 px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-700 focus:outline-none"
+                            onClick={() => navigate("/dashboard")}
+                        >
+                            Direct to dashboard
+                        </button>
+                    )}
+                </CustomModal>
+                <CustomModal
+                    isOpen={roleModalIsOpen}
+                    onClose={() => setRoleModalIsOpen(false)}
+                    title="Select Role"
+                    message="Please choose your role"
+                >
+                    <button
+                        className="mt-4 px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-700 focus:outline-none"
+                        onClick={() => handleRoleSelection('mentor')}
+                    >
+                        Mentor
+                    </button>
+                    <button
+                        className="mt-4 ml-2 px-4 py-2 text-white bg-green-500 rounded-md hover:bg-green-700 focus:outline-none"
+                        onClick={() => handleRoleSelection('mentee')}
+                    >
+                        Mentee
+                    </button>
+                </CustomModal>
             </div>
         </div>
     );
