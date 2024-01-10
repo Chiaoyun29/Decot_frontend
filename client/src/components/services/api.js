de3629f12285188c388124c1a53f39764dfc6e85
wasdqwerfzxcv <72960153+wasdqwerfzxcv@users.noreply.github.com>
2024-01-11 04:03:28 +0800

    update heroku url

--- client/src/components/services/api.js
+++ client/src/components/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://decot.azurewebsites.net';
+const API_URL = 'https://decot-41f64098cefb.herokuapp.com';
 
 export const registerUser = async (username, email, password, role) => {
   try {
