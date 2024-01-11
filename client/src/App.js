4a988c68e7246a76b02a179740b6181d78d2ec24
wasdqwerfzxcv <72960153+wasdqwerfzxcv@users.noreply.github.com>
2024-01-11 21:27:06 +0800

    replace browser router with hash router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Register from './pages/auth/Register';
 import Login from './pages/auth/Login';
