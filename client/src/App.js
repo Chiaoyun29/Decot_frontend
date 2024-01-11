cfb4250d5e54cf7fc1d1ee06dba575d6c492e8dd
wasdqwerfzxcv <72960153+wasdqwerfzxcv@users.noreply.github.com>
2024-01-11 21:41:51 +0800

    update solve refresh way

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Register from './pages/auth/Register';
 import Login from './pages/auth/Login';
